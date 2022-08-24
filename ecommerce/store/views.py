c80a334916e0803c739b8d268972e6f4a176fa23
Sin0bee <johndoe@example.com>
2022-08-24 10:54:23 +0200

    first commit

---
+++ ecommerce/store/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def store(request):
+    context = {}
+    render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    render(request, 'store/checkout.html', context)
